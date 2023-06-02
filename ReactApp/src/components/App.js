1f81ab9fdc55149d3b8e2fffc1471e9d1f941b13
Teviets <est21405@uvg.edu.gt>
2023-06-02 13:29:11 -0600

    Inicio de router

--- ReactApp/src/components/App.js
+++ ReactApp/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import NavBar from './NavBar/NavBar';
+import Catalogo from './catalogo/catalogo';
+
+import {Routes, Route, Link} from 'react-router-dom';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <NavBar/>
+                <Routes>
+                    <Route path='/'></Route>
+                    <Route path='/catalogo'></Route>
+                    <Route path='/acerca'></Route>
+                </Routes>
             </div>
         );
     }
--- ReactApp/src/index.js
+++ ReactApp/src/index.js
@@ -6,7 +6,12 @@ import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@mdi/font/css/materialdesignicons.css';
+import {BrowserRouter} from 'react-router-dom';
 
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+    , document.getElementById('root'));
