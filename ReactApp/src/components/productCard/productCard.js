5fbf7dea7d1e4fa2f6e48bfdbe78d8d18bfcd562
Teviets <est21405@uvg.edu.gt>
2023-06-02 21:16:07 -0600

    x

--- ReactApp/src/components/productCard/productCard.js
+++ ReactApp/src/components/productCard/productCard.js
@@ -5,7 +5,7 @@ export default function ProductCard(props) {
   return (
     <div className="product-card">
       <img src={props.img} alt="Producto 1" />
-      <div id="InfoProduct">
+      <div className="info-product">
         <p className="Texto1">{props.nombre}</p>
         <p className="SubTexto1">{props.precio}</p>
       </div>
