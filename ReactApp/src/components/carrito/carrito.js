b85b7411c98e9b3749a2e7931818c5d3e2734c2e
Sebastián Solorzano <sol21826@uvg.edu.gt>
2023-09-29 17:55:38 -0600

    Método de pago

--- ReactApp/src/components/carrito/carrito.js
+++ ReactApp/src/components/carrito/carrito.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Modal from 'react-bootstrap/Modal';
 import './carrito.css';
 
 function Carrito({ cart, setCart }) {
     const navigate = useNavigate();
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const handleRemoveProduct = (index) => {
         const updatedCart = [...cart];
         updatedCart.splice(index, 1);
         setCart(updatedCart); // Actualiza el estado del carrito
-    };    
+    };
 
     console.log("Productos en el carrito:", cart);
     const calculateCosts = () => {
@@ -95,7 +105,118 @@ function Carrito({ cart, setCart }) {
                             <button className="btn btn-block btn-light" onClick={handleClickCatalogo}>Continuar Comprando</button>
                         </div>
                         <div className="col-sm-12 col-md-6 text-right">
-                            <button className="btn btn-lg btn-block btn-success text-uppercase">Pagar</button>
+                            <button className="btn btn-lg btn-block btn-success text-uppercase" onClick={handleShow}>Pagar</button>
+
+                            <Modal show={show} onHide={handleClose} animation={false}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Orden de Pago</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form>
+                                        <Row className="mb-3">
+                                            <Form.Group as={Col} controlId="formGridName">
+                                                <Form.Label>Nombre</Form.Label>
+                                                <Form.Control type="name" placeholder="Nombre" />
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} controlId="formGridLastName">
+                                                <Form.Label>Apellido</Form.Label>
+                                                <Form.Control type="lastname" placeholder="Apellido" />
+                                            </Form.Group>
+                                        </Row>
+
+                                        <Row className="mb-3">
+                                            <Form.Group as={Col} controlId="formGridEmail">
+                                                <Form.Label>Correo</Form.Label>
+                                                <Form.Control type="email" placeholder="Correo" />
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} controlId="formGridPassword">
+                                                <Form.Label>Contraseña</Form.Label>
+                                                <Form.Control type="password" placeholder="Contraseña" />
+                                            </Form.Group>
+                                        </Row>
+
+                                        <Form.Group className="mb-3" controlId="formGridAddress1">
+                                            <Form.Label>Dirección 1</Form.Label>
+                                            <Form.Control placeholder="Calle, número de casa, zona" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formGridAddress2">
+                                            <Form.Label>Dirección 2</Form.Label>
+                                            <Form.Control placeholder="Apartamento, colonia, casa" />
+                                        </Form.Group>
+
+                                        <Row className="mb-3">
+                                            <Form.Group as={Col} controlId="formGridCity">
+                                                <Form.Label>Ciudad</Form.Label>
+                                                <Form.Control placeholder="Guatemala" />
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} controlId="formGridState">
+                                                <Form.Label>Departamento</Form.Label>
+                                                <Form.Select defaultValue="Choose...">
+                                                    <option>Escoger...</option>
+                                                    <option>...</option>
+                                                </Form.Select>
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} controlId="formGridZip">
+                                                <Form.Label>Código postal</Form.Label>
+                                                <Form.Control />
+                                            </Form.Group>
+                                        </Row>
+
+                                        <fieldset>
+                                            <h6>Tipo de tarjeta</h6>
+                                            <Form.Group as={Row} className="mb-3">
+                                                <Col sm={10}>
+                                                    <Form.Check
+                                                        type="radio"
+                                                        label="Tarjeta de crédito"
+                                                        name="formHorizontalRadios"
+                                                        id="formHorizontalRadios1"
+                                                    />
+                                                    <Form.Check
+                                                        type="radio"
+                                                        label="Tarjeta de débito"
+                                                        name="formHorizontalRadios"
+                                                        id="formHorizontalRadios2"
+                                                    />
+                                                </Col>
+                                            </Form.Group>
+                                        </fieldset>
+
+                                        <Form.Group className="mb-3" controlId="formGridNameCard">
+                                            <Form.Label>Nombre de la tarjeta</Form.Label>
+                                            <Form.Control placeholder="Nombre" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formGridCreditNumber">
+                                            <Form.Label>Número de tarjeta</Form.Label>
+                                            <Form.Control placeholder="" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formGrid">
+                                            <Form.Label>Fecha de vencimiento</Form.Label>
+                                            <Form.Control placeholder="" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formGridAddress2">
+                                            <Form.Label>Código de seguridad (CVV)</Form.Label>
+                                            <Form.Control placeholder="" />
+                                        </Form.Group>
+                                    </Form>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose} style={{ backgroundColor: '#B2B6BD', borderColor: '#B2B6BD' }}>
+                                        Cancelar
+                                    </Button>
+                                    <Button variant="primary" onClick={handleClose} style={{ backgroundColor: '#198754', borderColor: '#198754' }}>
+                                        Realizar pago
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
                         </div>
                     </div>
                 </div>
