274ed271e4f934e1568d7624254b670577ade4c1
Sebastián Solorzano <sol21826@uvg.edu.gt>
2023-11-09 21:28:17 -0600

    Datos

--- ReactApp/src/components/catalogo/catalogo.js
+++ ReactApp/src/components/catalogo/catalogo.js
@@ -5,10 +5,6 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import './catalogo.css';
 
-import arreF1 from '../../assets/Flores/ArregloF/ArregloF1.jpeg';
-import arreF2 from '../../assets/Flores/ArregloF/ArregloF2.jpeg';
-import arreF3 from '../../assets/Flores/ArregloF/ArregloF3.jpeg';
-
 function Catalogo({ addToCart }) {
   const [searchTerm, setSearchTerm] = useState('');
   const [showFilters, setShowFilters] = useState(false);
@@ -24,13 +20,13 @@ function Catalogo({ addToCart }) {
       const categoriesResponse = await fetch('http://localhost:3161/categorias');
       const categories = await categoriesResponse.json();
       const allProducts = {};
-  
+
       for (const category of categories) {
         const productsResponse = await fetch(`http://localhost:3161/arreglos/${category.id_categoria}`);
         const products = await productsResponse.json();
         allProducts[category.id_categoria] = products;
       }
-  
+
       setProducts(allProducts);
       console.log('Products:', allProducts);
     } catch (error) {
@@ -59,14 +55,6 @@ function Catalogo({ addToCart }) {
       [category]: !prevCategories[category],
     }));
   };
-
-  const filteredProductsArreglosF = [
-    { img: arreF1, nombre: 'Arreglo de Flores', precio: 'Q 150.00', categoria: 'ArreglosF' },
-    { img: arreF2, nombre: 'Arreglo de Flores', precio: 'Q 200.00', categoria: 'ArreglosF' },
-    { img: arreF3, nombre: 'Arreglo de Flores', precio: 'Q 250.00', categoria: 'ArreglosF' },
-  ].filter((product) =>
-    product.nombre.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   const filterProducts = (categoryId) => {
     const categoryProducts = products[categoryId];
@@ -152,13 +140,13 @@ function Catalogo({ addToCart }) {
           <h1>Arreglos de Flores</h1>
         )}
         {showCategories.ArreglosF && (
+          console.log(products[1]),
           <div className="product-container">
-            {filteredProductsArreglosF.map((product, index) => (
+            {filterProducts(1).map((product, index) => (
               <div className={`product${index + 1}`} key={index}>
                 <ProductCard
-                  nombre={product.nombre}
-                  precio={product.precio}
-                  categoria={product.categoria}
+                  nombre={product["Nombre del Arreglo"]}
+                  precio={'Q'+product.Precio+'.00'}
                   addToCart={addToCart}
                   handleShow={handleShow}
                 />
@@ -178,8 +166,8 @@ function Catalogo({ addToCart }) {
             {filterProducts(2).map((product, index) => (
               <div className={`product${index + 1}`} key={index}>
                 <ProductCard
-                  nombre={product.nombre}
-                  precio={product.precio}
+                  nombre={product["Nombre del Arreglo"]}
+                  precio={'Q'+product.Precio+'.00'}
                   addToCart={addToCart}
                   handleShow={handleShow}
                 />
@@ -199,8 +187,8 @@ function Catalogo({ addToCart }) {
             {filterProducts(3).map((product, index) => (
               <div className={`product${index + 1}`} key={index}>
                 <ProductCard
-                  nombre={product.nombre}
-                  precio={product.precio}
+                  nombre={product["Nombre del Arreglo"]}
+                  precio={'Q'+product.Precio+'.00'}
                   addToCart={addToCart}
                   handleShow={handleShow}
                 />
@@ -220,8 +208,8 @@ function Catalogo({ addToCart }) {
             {filterProducts(4).map((product, index) => (
               <div className={`product${index + 1}`} key={index}>
                 <ProductCard
-                  nombre={product.nombre}
-                  precio={product.precio}
+                  nombre={product["Nombre del Arreglo"]}
+                  precio={'Q'+product.Precio+'.00'}
                   addToCart={addToCart}
                   handleShow={handleShow}
                 />
@@ -241,8 +229,8 @@ function Catalogo({ addToCart }) {
             {filterProducts(5).map((product, index) => (
               <div className={`product${index + 1}`} key={index}>
                 <ProductCard
-                  nombre={product.nombre}
-                  precio={product.precio}
+                  nombre={product["Nombre del Arreglo"]}
+                  precio={'Q'+product.Precio+'.00'}
                   addToCart={addToCart}
                   handleShow={handleShow}
                 />
